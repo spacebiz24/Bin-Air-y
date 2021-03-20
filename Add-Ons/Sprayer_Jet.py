929075caf7488181f909f3859bc4e84f40e1b2f6
Siddhaanth S Iyer <78657717+spacebiz24@users.noreply.github.com>
2021-03-21 02:11:50 +0530

    Create Sprayer_Jet.py

---
+++ Add-Ons/Sprayer_Jet.py
@@ -0,0 +1,20 @@
+# This uses the smbus library to use I2C with the Jetson
+
+from smbus import SMBus
+
+addr = 0x8
+bus = SMBus
+
+numb = 1
+
+# Example code which takes input and sends data to the Arduino
+print("Enter 1 for starting the sprayer and 0 for turning it off")
+while (numb == 1):
+  spray = input(">>")
+  
+  if spray == "1":
+    bus.write_byte(addr, 0x1) # Turns on sprayer
+  elif spray == "0":
+    bus.write_byte(addr, 0x0) # Turns off sprayer
+  else :
+    numb = 0
